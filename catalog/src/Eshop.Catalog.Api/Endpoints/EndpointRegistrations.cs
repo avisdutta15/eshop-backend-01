f76bf026985b98130da78eeaf13482da47c51892
Avishek Dutta <avishek.dutta@maersk.com>
2024-05-18 18:42:54 +0530

    Structured the api project into endpoints

---
+++ catalog/src/Eshop.Catalog.Api/Endpoints/EndpointRegistrations.cs
@@ -0,0 +1,22 @@
+﻿using Eshop.Catalog.Api.Endpoints.WeatherForecast;
+
+namespace Eshop.Catalog.Api.Endpoints;
+
+public static class EndpointRegistrations
+{
+    public static WebApplication UseCatalogEndpoints(this WebApplication app)
+    {
+        app.UseWeatherEndpoints();
+        return app;
+    }
+
+    public static WebApplication UseWeatherEndpoints(this WebApplication app)
+    {
+        var weatherForecastGroup = app.MapGroup("/weatherforecast");
+
+        weatherForecastGroup.MapGet("/", GetWeatherForecast.Handle)
+                            .WithName("GetWeatherForecast")
+                            .WithOpenApi();
+        return app;
+    }
+}
---
+++ catalog/src/Eshop.Catalog.Api/Endpoints/WeatherForecast/GetWeatherForecast.cs
@@ -0,0 +1,29 @@
+﻿namespace Eshop.Catalog.Api.Endpoints.WeatherForecast; 
+
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+public static class GetWeatherForecast
+{
+    public static async Task<Ok<WeatherForecast[]>> Handle()
+    {
+        var summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+                .ToArray();
+        return TypedResults.Ok(forecast);
+    }
+}
