c0bfdf9b65dc65920ca51b685526428db32f71cc
Avishek Dutta <avishek.dutta@maersk.com>
2024-05-26 00:40:32 +0530

    reverting usmi client id

--- catalog/src/Eshop.Catalog.Services/ServiceCollectionExtensions.cs
+++ catalog/src/Eshop.Catalog.Services/ServiceCollectionExtensions.cs
@@ -8,7 +8,8 @@ using Npgsql;
 
 public static class ServiceCollectionExtensions
 {    
-    private static readonly DefaultAzureCredential AzureCredentials = new(new DefaultAzureCredentialOptions { ManagedIdentityClientId = "2ed28938-26f9-402e-a21c-68512cfbd671" });
+    //private static readonly DefaultAzureCredential AzureCredentials = new(new DefaultAzureCredentialOptions { ManagedIdentityClientId = "2ed28938-26f9-402e-a21c-68512cfbd671" });
+    private static readonly DefaultAzureCredential AzureCredentials = new();
     private static readonly TokenRequestContext OssrdbmsTokenRequest = new(
     [
         "https://ossrdbms-aad.database.windows.net/.default"
