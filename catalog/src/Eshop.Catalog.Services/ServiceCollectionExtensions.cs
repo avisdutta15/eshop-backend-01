f41f0033842a250e7fe94e02cd4722b59f82c74c
Avishek Dutta <avishek.dutta@maersk.com>
2024-05-26 00:36:12 +0530

    passing the usmi client id to the default credential

--- catalog/src/Eshop.Catalog.Services/ServiceCollectionExtensions.cs
+++ catalog/src/Eshop.Catalog.Services/ServiceCollectionExtensions.cs
@@ -7,8 +7,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
 
 public static class ServiceCollectionExtensions
-{
-    private static readonly DefaultAzureCredential AzureCredentials = new();
+{    
+    private static readonly DefaultAzureCredential AzureCredentials = new(new DefaultAzureCredentialOptions { ManagedIdentityClientId = "2ed28938-26f9-402e-a21c-68512cfbd671" });
     private static readonly TokenRequestContext OssrdbmsTokenRequest = new(
     [
         "https://ossrdbms-aad.database.windows.net/.default"
@@ -33,6 +33,7 @@ public static class ServiceCollectionExtensions
             .UsePeriodicPasswordProvider(async (builder, ct) =>
             {
                 var accessToken = await AzureCredentials.GetTokenAsync(OssrdbmsTokenRequest, ct);
+                Console.WriteLine(accessToken.Token);
                 return accessToken.Token;
             }, TimeSpan.FromMinutes(55), TimeSpan.FromSeconds(5))
             .Build();
